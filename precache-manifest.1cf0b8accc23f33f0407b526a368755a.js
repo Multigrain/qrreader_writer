8846be441ceeaba4e342afb14d04ab79ebc72cdd
Ian <DTAwarior@hotmail.com>
2020-06-27 04:16:20 -0400

    Updates

---
+++ precache-manifest.1cf0b8accc23f33f0407b526a368755a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f3594cf355356770772f0cb1d1fdd3",
+    "url": "/qrreader_writer/index.html"
+  },
+  {
+    "revision": "ecf9ee10b789f0c4a610",
+    "url": "/qrreader_writer/static/css/main.d68457c3.chunk.css"
+  },
+  {
+    "revision": "e5e28dc38749654a3d4e",
+    "url": "/qrreader_writer/static/js/2.cf14d14e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/qrreader_writer/static/js/2.cf14d14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf9ee10b789f0c4a610",
+    "url": "/qrreader_writer/static/js/main.dbab941a.chunk.js"
+  },
+  {
+    "revision": "58f2a3527c31779fa33a",
+    "url": "/qrreader_writer/static/js/runtime-main.532c4183.js"
+  }
+]);
